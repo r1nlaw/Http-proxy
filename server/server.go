fd45ee9e84a6a3e94a46b9542bb31a54b4196682
r1nlaw <gru.zhizhi@mail.ru>
2025-05-10 15:14:53 +0300

    http proxy

--- server/server.go
+++ server/server.go
@@ -6,22 +6,15 @@ import (
 	"net/http"
 )
 
-// Обработчик для загрузки файла index.html
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Устанавливаем CORS заголовки для всех запросов
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	// Обслуживаем файл index.html
 	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
-	// Регистрируем обработчик
 	http.HandleFunc("/", handler)
-
-	// Запускаем сервер на порту 8085
 	port := "8085"
 	fmt.Printf("Starting server on http://localhost:%s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
